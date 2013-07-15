de221bc0fcb510291a3c6872dcca8874ae86a905
Matthew Krueger <mkrueger@Administrators-MacBook-Pro.local>
2013-07-15 12:33:56 -0400

    init

---
+++ ArthrexAPI/ArthrexAPI.m
@@ -0,0 +1,18 @@
+//
+//  ArthrexAPI.m
+//  ArthrexAPI
+//
+//  Created by Matthew Krueger on 7/12/13.
+//  Copyright (c) 2013 Arthrex. All rights reserved.
+//
+
+#import "ArthrexAPI.h"
+
+@implementation ArthrexAPI
+
+- (void)testCustomFramework
+{
+    NSLog(@"Testing custom framewotk");
+}
+
+@end
---
+++ Products/ArthrexAPI.framework/Headers/ArthrexAPI.h
@@ -0,0 +1,15 @@
+//
+//  ArthrexAPI.h
+//  ArthrexAPI
+//
+//  Created by Matthew Krueger on 7/12/13.
+//  Copyright (c) 2013 Arthrex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ArthrexAPI : NSObject
+
+- (void)testCustomFramework;
+
+@end
